a3cc51d9c3ab57b56771c664fbc080cf0908888a
AliAhmad-0 <aliahmad554327@gmail.com>
2024-08-25 17:43:25 +0500

    Area of rectangle

---
+++ main.py
@@ -0,0 +1,47 @@
+""" length = 20
+width = 10 """
+
+""" area = length*width """
+
+""" def areOfRectangle():
+    length = int(input("Enter Length of Rectangle: "))
+    width = int(input("Enter width of rectangle: "))
+    return length*width
+area = areOfRectangle()
+print(area) """
+
+
+
+
+
+
+""" 
+def areaOfRectangule(length,width):
+    area = length*width
+    return area
+
+areaRectangle = areaOfRectangule(10,20)
+print(areaRectangle) """
+
+
+
+
+""" def areaOfRectangle():
+    length = int(input("enter length of rectangle: "))
+    width = int(input ("enter width of the rectangle: "))
+    area = length * width 
+    return area
+
+rectangleArea = areaOfRectangle()
+print(rectangleArea) """
+    
+    
+    
+
+
+
+
+
+
+
+
